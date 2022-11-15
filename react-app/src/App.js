84e895a3b7653d0e3122190cb81ec780faaf1b8d
seonghyuk1 <tjdgur0737@naver.com>
2022-11-15 13:05:55 +0900

    test

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,24 @@
-import Login from './Pages/Login';
-import Main from './Pages/Main'
-import Signup from './Pages/Signup'
+import Login from "./Pages/Login";
+import Main from "./Pages/Main";
+import Signup from "./Pages/Signup";
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={ <Login/> } />
-      <Route path='signup' element={ <Signup/> } />
-      <Route path='Main' element={ <Main/> } />
-    </Routes>
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand m-3" href="/">
+            우리는 오늘 어떤 추억을 쌓을까?
+          </a>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="Main" element={<Main />} />
+      </Routes>
     </>
   );
 }
