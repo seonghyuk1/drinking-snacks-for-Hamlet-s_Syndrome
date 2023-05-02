606f795c7d6fcb9511ca5f467c9d44ae298908b9
seonghyuk1 <tjdgur0737@naver.com>
2023-05-02 23:34:24 +0900

    Ready for refactoring

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
       </div>
       <div>
         <span className="FooterText"></span>
-        <span className="FooterText">ss경기도 성남시 분당구 대왕판교로 644번길 12 (우)13494 12, Daewangpangyo-ro 644beon-gil, Bundang-gu, Seongnam-si, Gyeonggi-do, Korea (13494). </span>
+        <span className="FooterText">경기도 성남시 분당구 대왕판교로 644번길 12 (우)13494 12, Daewangpangyo-ro 644beon-gil, Bundang-gu, Seongnam-si, Gyeonggi-do, Korea (13494). </span>
       </div>
     </>
   );
